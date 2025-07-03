396d9ff06134107b88963d165e039fa536006f3c
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-07-03 17:38:49 -0400

    feat: commonjs exports/imports

---
+++ formula.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply =  (a, b) => a * b;
+// const devide = (a, b) => a / b;
+const devide = (a, b) => a / b;
+
+// const milesToKilometers = (miles) => miles * 1.60934;
+const milesToKilometers = (miles) => miles * 1.60934;
+const celsiusToFahrenheit = (celsius) => (celsius * 9) / 5 + 32;
+
+module.exports = { add, subtract, multiply, devide, milesToKilometers, celsiusToFahrenheit }
--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-console.log('Hello people!')
+// console.log('Hello people!')
 // console.log(global)
 
-const os = require('os')
-const path = require('path')
+// const os = require('os')
+// const path = require('path')
 
-console.log(os.type())
-console.log(os.version())
-console.log(os.homedir())
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
 
-console.log(__dirname)
-console.log(__filename)
+// console.log(__dirname)
+// console.log(__filename)
 
-console.log(path.dirname(__filename))
-console.log(path.basename(__filename))
-console.log(path.extname(__filename))
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
 
-console.log(path.parse(__filename))
+// console.log(path.parse(__filename))
+
+const formula = require('./formula')
+const { devide, milesToKilometers } = require('./formula')
+
+console.log(formula.celsiusToFahrenheit(21))
+console.log(formula.subtract(10, 7))
+console.log(formula.multiply(8, 9))
+console.log(devide(25, 5))
+console.log(milesToKilometers(10))
+
