9537dd136db119b98cb7f7d17e12b871d4301c38
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-07-05 10:52:54 -0400

    feat: fs methods with async await way

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-const fs = require('fs')
+const fsPromises = require('fs').promises
 const path = require('path')
 
-fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
-    if (err) throw err
-    console.log(data) 
-})
+const fileOps = async () => {
+    try {
+        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8')
+        console.log(data)
+        await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'))
+        await fsPromises.writeFile(path.join(__dirname, 'files', 'reply.txt'), data)
+        await fsPromises.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nAppended text to starter file')
+        await fsPromises.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'))
+        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'newReply.txt'), 'utf8')
+        console.log(newData)
+    } catch (err) {
+        console.error(err)
+    }
+}
 
-fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you!', (err) => {
-    if (err) throw err
-    console.log('Write complete')
+fileOps()
 
-    fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\n Nice to meet you too!', (err) => {
-        if (err) throw err
-        console.log('Append complete!')
+// fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
+//     if (err) throw err
+//     console.log(data) 
+// })
 
-        fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply'), (err) => {
-            if (err) throw err
-            console.log('Rename complete!')
-        })
-    })
-})
+// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you!', (err) => {
+//     if (err) throw err
+//     console.log('Write complete')
 
-fs.appendFile(path.join(__dirname, 'files', 'test.txt'), '\n\n Has it appended the text?', (err) => {
-    if (err) throw err
-    console.log('AppendFile complete!')
-})
+//     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\n Nice to meet you too!', (err) => {
+//         if (err) throw err
+//         console.log('Append complete!')
+
+//         fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply'), (err) => {
+//             if (err) throw err
+//             console.log('Rename complete!')
+//         })
+//     })
+// })
 
 
 // Exit on uncaught errors
