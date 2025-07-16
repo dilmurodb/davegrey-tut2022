4631a365b86f0a33fef9deca03894433a19d7c9a
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-07-16 15:53:30 -0400

    feat: middleware

--- server.js
+++ server.js
@@ -1,28 +1,67 @@
-// console.log('Hello people!')
-// console.log(global)
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const path = require('path');
+const { logger } = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
+const PORT = process.env.PORT || 3500;
 
-// const os = require('os')
-// const path = require('path')
 
-// console.log(os.type())
-// console.log(os.version())
-// console.log(os.homedir())
+// Custom middleware logger
+app.use(logger);
 
-// console.log(__dirname)
-// console.log(__filename)
+// Cross Origin Resource Sharing (CORS)
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not Allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
 
-// console.log(path.dirname(__filename))
-// console.log(path.basename(__filename))
-// console.log(path.extname(__filename))
+}
 
-// console.log(path.parse(__filename))
+app.use(cors(corsOptions));
 
-const formula = require('./formula')
-const { devide, milesToKilometers } = require('./formula')
+// Buil in middleware to handle urlencoded data
+// in other words, form data:
+// 'Content-Type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
 
-console.log(formula.celsiusToFahrenheit(21))
-console.log(formula.subtract(10, 7))
-console.log(formula.multiply(8, 9))
-console.log(devide(25, 5))
-console.log(milesToKilometers(10))
+// Built in middleware for json
+app.use(express.json());
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get(/^\/$|^\/index(?:\.html)?$/ , (req, res) => {
+    // res.sendFile('./views/index.html', { root: __dirname })
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+});
+
+app.get( /^\/new-page(?:\.html)?$/ , (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+});
+
+app.get(/^\/old-page(?:.html)?$/ , (req, res) => {
+    res.redirect('/new-page.html')
+});
+
+app.get(/^.*$/ , (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json( { error: "404 Not Found"})
+    } else {
+        res.type('txt').send("404 Not Found")
+    }
+});
+
+app.use(errorHandler)
+
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) });
