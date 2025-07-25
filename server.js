d5ea97f3920e027b3f5dff552ba161220bd05338
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-07-25 11:26:34 -0400

    feat: implement register and authorization functionality

--- server.js
+++ server.js
@@ -24,12 +24,14 @@ app.use(express.json());
 
 // Serve static files
 app.use('/', express.static(path.join(__dirname, 'public')));
-app.use('/subdir', express.static(path.join(__dirname, 'public')));
+// app.use('/subdir', express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', require('./routes/root'));
-app.use('/subdir', require('./routes/subdir'));
-app.use('/employees', require('./routes/api/employees'))
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+// app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 
 app.get(/^.*$/ , (req, res) => {
@@ -43,6 +45,6 @@ app.get(/^.*$/ , (req, res) => {
     }
 });
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) });
